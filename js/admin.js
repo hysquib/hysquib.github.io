c9a1fd80bec009489ec88e752a274b9cf5ec378b
hysquib <hongyaojiajin@foxmail.com>
2026-08-05 13:52:13 +0800

    fix: 修正 admin.js 正则表达式语法错误

--- js/admin.js
+++ js/admin.js
@@ -672,7 +672,7 @@ const AdminApp = {
             .replace(/\*(.+?)\*/g, '$1')
             .replace(/`(.+?)`/g, '$1')
             .replace(/\[(.+?)\]\(.+?\)/g, '$1')
-            .replace(!\[.*?\]\(.+?\)/g, '')
+            .replace(/!\[.*?\]\(.+?\)/g, '')
             .replace(/^\s*[-*]\s+/gm, '')
             .replace(/^\s*\d+\.\s+/gm, '')
             .replace(/>\s+/gm, '')
